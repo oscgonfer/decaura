0793faa35a7d89d67b63deff9d81c9b233f9a8bd
oscgonfer <oscgonfer@gmail.com>
2023-05-21 10:52:34 +0200

    Modify async.py to be more generic

--- async.py
+++ async.py
@@ -8,14 +8,26 @@ from threading import current_thread
 from queue import Queue
 from typing import List, Any
 import RPi.GPIO as GPIO
-LED_PIN = 17
+LAMP_1_PIN = 17
+LAMP_2_PIN = 27
+LAMPS = [LAMP_1_PIN, LAMP_2_PIN]
+INVERTED = True
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(True)
-GPIO.setup(LED_PIN, GPIO.OUT)
+for lamp in LAMPS:
+    GPIO.setup(lamp, GPIO.OUT)
+
+if INVERTED:
+    HIGH = GPIO.LOW
+    LOW = GPIO.HIGH
+else:
+    HIGH = GPIO.HIGH
+    LOW = GPIO.LOW
 
 SERVER_IP = "decaura.local"
 SERVER_PORT = 6005
-UDP_FILTER = "/hello/*"
+UDP_FILTER = "/lamp/*"
 
 def std_out(msg, who):
     print (f'[{who}]: {msg}')
@@ -24,7 +36,7 @@ def std_out(msg, who):
 async def loop():
     while True:
         # This can't be 0, as it will load the cpu like crazy...
-        await asyncio.sleep(0.001)
+        await asyncio.sleep(0.01)
 
 class UDPBridge(object):
     def __init__(self):
@@ -36,16 +48,22 @@ class UDPBridge(object):
                                      asyncio.get_event_loop())
         # Create datagram endpoint and start serving
         transport, protocol = await server.create_serve_endpoint()
-
+        std_out('Launching server', 'OSC')
         await loop()
         transport.close()  # Clean up serve endpoint
 
     def send(self, *args: List[Any]) -> None:
-        std_out (f'{time.strftime("%H:%M:%S %d-%m-%Y")}: {args[0]}\n', 'SACN')
-        GPIO.output(LED_PIN, GPIO.HIGH)
-        time.sleep(0.1)
-        GPIO.output(LED_PIN, GPIO.LOW)
+        std_out (f'{time.strftime("%H:%M:%S %d-%m-%Y")}: {args[0]}', 'OSC')
         
+        try:
+            lamp = int(args[0].split('/')[2])-1
+        except:
+            print ('Message malformed')
+            pass
+        else:
+            GPIO.output(LAMPS[lamp], HIGH)
+            time.sleep(0.1)
+            GPIO.output(LAMPS[lamp], LOW)
 
 udpbridge = UDPBridge()
 dispatcher = Dispatcher()
